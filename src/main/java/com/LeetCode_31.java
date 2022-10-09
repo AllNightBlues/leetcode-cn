d89b8af511f996c2a50508444f36130f21031aa8
AllNightBlues <stevezlh@hotmail.com>
2022-10-09 20:11:44 +0800

    add: LeetCode_31

---
+++ src/main/java/com/LeetCode_31.java
@@ -0,0 +1,46 @@
+/**
+ * @(#) LeetCode_31.java 1.0 2022-10-09
+ * Copyright (c) 2022, AllNightBlues. ALL right reserved.
+ * AllNightBlues PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com;
+
+/**
+ * @ClassName LeetCode_31
+ * @description: leetcode-cn-31 
+ * @AUTHOR AllNightBlues
+ * @Date 2022/10/09 19:52
+ * @Version 1.0
+ **/
+public class LeetCode_31 {
+
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
