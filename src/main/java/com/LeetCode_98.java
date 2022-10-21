94775bc9df3cdbe6c1e59ada52f1c2d4c24c37b2
AllNightBlues <stevezlh@hotmail.com>
2022-10-21 11:11:57 +0800

    add: LeetCode_98

---
+++ src/main/java/com/LeetCode_98.java
@@ -0,0 +1,43 @@
+/**
+ * @(#) LeetCode_98.java 1.0 2022-10-21
+ * Copyright (c) 2022, AllNightBlues. ALL right reserved.
+ * AllNightBlues PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com;
+
+/**
+ * @ClassName LeetCode_98
+ * @description:
+ * @AUTHOR AllNightBlues
+ * @Date 2022/10/21 11:04
+ * @Version 1.0
+ **/
+public class LeetCode_98 {
+
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;}
+     }
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val >= maxVal || root.val <= minVal) {
+            return false;
+        }
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+    }
+}
