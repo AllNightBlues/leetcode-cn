5298f0fef9ced417f2fd977ce9918af1fcf19021
AllNightBlues <stevezlh@hotmail.com>
2022-10-21 10:56:57 +0800

    add: LeetCode_224

---
+++ src/main/java/com/LeetCode_224.java
@@ -0,0 +1,54 @@
+/**
+ * @(#) LeetCode_224.java 1.0 2022-10-21
+ * Copyright (c) 2022, AllNightBlues. ALL right reserved.
+ * AllNightBlues PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @ClassName LeetCode_224
+ * @description:
+ * @AUTHOR AllNightBlues
+ * @Date 2022/10/21 10:50
+ * @Version 1.0
+ **/
+public class LeetCode_224 {
+
+    public int calculate(String s) {
+        Deque<Integer> ops = new LinkedList<Integer>();
+        ops.push(1);
+        int sign = 1;
+
+        int ret = 0;
+        int n = s.length();
+        int i = 0;
+        while (i < n) {
+            if (s.charAt(i) == ' ') {
+                i++;
+            } else if (s.charAt(i) == '+') {
+                sign = ops.peek();
+                i++;
+            } else if (s.charAt(i) == '-') {
+                sign = -ops.peek();
+                i++;
+            } else if (s.charAt(i) == '(') {
+                ops.push(sign);
+                i++;
+            } else if (s.charAt(i) == ')') {
+                ops.pop();
+                i++;
+            } else {
+                long num = 0;
+                while (i < n && Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + s.charAt(i) - '0';
+                    i++;
+                }
+                ret += sign * num;
+            }
+        }
+        return ret;
+    }
+}
