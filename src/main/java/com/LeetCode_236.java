152f6e7383644017a7f930810b80d9eb47cce444
AllNightBlues <stevezlh@hotmail.com>
2022-10-24 15:36:52 +0800

    add: LeetCode_236

---
+++ src/main/java/com/LeetCode_236.java
@@ -0,0 +1,32 @@
+/**
+ * @(#) LeetCode_236.java 1.0 2022-10-24
+ * Copyright (c) 2022, AllNightBlues. ALL right reserved.
+ * AllNightBlues PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com;
+
+/**
+ * @ClassName LeetCode_236
+ * @description:
+ * @AUTHOR AllNightBlues
+ * @Date 2022/10/24 11:23
+ * @Version 1.0
+ **/
+public class LeetCode_236 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left == null) return right;
+        if(right == null) return left;
+        return root;
+    }
+}
