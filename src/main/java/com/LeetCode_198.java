0886f1cfe0132024eb8bb20581375b2721e3b916
AllNightBlues <stevezlh@hotmail.com>
2022-11-20 11:48:39 +0800

    add: LeetCode_198

---
+++ src/main/java/com/LeetCode_198.java
@@ -0,0 +1,29 @@
+/**
+ * @(#) LeetCode_198.java 1.0 2022-11-20
+ * Copyright (c) 2022, AllNightBlues. ALL right reserved.
+ * AllNightBlues PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com;
+
+/**
+ * @ClassName LeetCode_198
+ * @description:
+ * @AUTHOR AllNightBlues
+ * @Date 2022/11/20 11:13
+ * @Version 1.0
+ **/
+public class LeetCode_198 {
+
+    public int rob(int[] nums) {
+        int length = nums.length;
+        if (length == 1) return nums[0];
+        if (length == 2) return Math.max(nums[0], nums[1]);
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+        return dp[length - 1];
+    }
+}
